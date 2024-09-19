004d7051779c9c357449bd33ca0b4c29c754e9d9
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-09-19 17:52:12 -0600

    loop practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator= new Random() ;
+       int secretNumber= randomGenerator.Next (1,100);
+       int numGuesses=0;
+       int secretGuess=0;
+       while (secretNumber!=secretGuess){
+       Console.WriteLine("Enter your guess for the secret number 1-100 ");
+       string guess=Console.ReadLine();
+        secretGuess=int.Parse(guess);
+       
+        if (secretGuess<secretNumber)
+        {
+            Console.WriteLine("The secret number is higer");
+            ++numGuesses;
+        }
+        if (secretGuess>secretNumber)
+        {
+            Console.WriteLine("The scecret number is lower");
+            ++numGuesses;
+        }
+       }
+       Console.WriteLine($"{secretNumber} is the correct answer.  It took you {numGuesses} guesses");
+
     }
 }
