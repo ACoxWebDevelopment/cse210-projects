705157f7b1a0278121127be19bfe3304c65bc56a
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-09-18 18:08:09 -0600

    if else practice

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Please enter your grade percentage as a whole number ");
+        string userInput=Console.ReadLine();
+        int grade= int.Parse(userInput);
+        string letter;
+        if (grade >= 90)
+            {
+               // Console.WriteLine("Your grade is an A");
+                letter="A";
+            }
+        else if (grade>=80)
+        {
+                 // Console.WriteLine("Your grade is a B");
+                letter="B";
+        }
+        else if (grade>=70)
+        {
+                 // Console.WriteLine("Your grade is a C");
+                letter="C";
+        }
+        else if (grade>=60)
+        {
+                 // Console.WriteLine("Your grade is a D");
+                letter="D";
+        }
+        else 
+        {
+                 // Console.WriteLine("Your grade is an F");
+                letter="F";
+        }
+        Console.WriteLine ($"Your grade is a/an {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine ("Congratulations you passed");
+        }
+        else
+        {
+            Console.WriteLine("You were not successful this time, but don't give up!");
+        }
+    }   
+
 }
