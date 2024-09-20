ad2120674e120ab4b52019db2323edcdbea47c71
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-09-19 19:52:53 -0600

    list practice

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> userNumbers= new List<int>();
+        int numInput=999;
+        string entry;
+        do 
+        {
+        Console.Write("Enter a number to add to the list or 0 to end ");
+        entry=Console.ReadLine();
+        numInput=int.Parse(entry);
+        userNumbers.Add(numInput);
+        }
+         while (numInput!=0);
+         userNumbers.Remove(0);
+         Console.WriteLine($"The Sum of these numbers is {userNumbers.Sum()}");
+         float average = userNumbers.Sum()/(float) userNumbers.Count;
+         Console.WriteLine($"The Average of these numbers is {average}");
+         Console.WriteLine($"The largest number is {userNumbers.Max()}");
     }
 }
