e3002f395298deaa32d7da4836a028b3e9b42019
Allan Cox <Allan.L.Coxrr@gmail.com>
2024-09-20 21:35:15 -0600

    function practice

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int inputnum)
+    {
+        return inputnum * inputnum;
+    }
+
+    static void Main()
+    {
+        DisplayWelcome();
+        string alias = PromptUserName();
+        int favnum = PromptUserNumber();
+        int numsquared = SquareNumber(favnum);
+        Console.WriteLine($"{alias}, your number squared is {numsquared}");
     }
 }
