bffab4454ab7cbaeaba469357182e11c4dda26bc
Allan Cox <Allan.L.Coxrr@gmail.com>
2025-05-03 15:43:32 -0600

    c# basics review 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,13 +1,14 @@
 using System;
 
 class Program
-{
+
+    {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
+        Console.WriteLine("What is your first name? ");
         string fname=Console.ReadLine();
-        Console.Write("What is your last name? ");
+        Console.WriteLine("What is your last name? ");
         string lname=Console.ReadLine();
-        Console.WriteLine ($"Your name is {lname}, {fname} {lname}.");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}");
     }
 }
