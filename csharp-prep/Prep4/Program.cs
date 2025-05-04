e860f5ec15448f992ed6e4734a322ac8a4600945
Allan Cox <Allan.L.Coxrr@gmail.com>
2025-05-03 20:38:32 -0600

    list practice

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,21 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> userNumbers= new List<int>();
-        int numInput=999;
-        string entry;
-        do 
+        List<int> numbers = new List<int>();
+        int entry;
+        do
         {
-        Console.Write("Enter a number to add to the list or 0 to end ");
-        entry=Console.ReadLine();
-        numInput=int.Parse(entry);
-        userNumbers.Add(numInput);
+            Console.WriteLine("Enter a number or 0 to quit");
+            entry=(Convert.ToInt32(Console.ReadLine()));
+            if (entry!=0)
+            {
+            numbers.Add(entry);
+            }
+
         }
-         while (numInput!=0);
-         userNumbers.Remove(0);
-         Console.WriteLine($"The Sum of these numbers is {userNumbers.Sum()}");
-         float average = userNumbers.Sum()/(float) userNumbers.Count;
-         Console.WriteLine($"The Average of these numbers is {average}");
-         Console.WriteLine($"The largest number is {userNumbers.Max()}");
+        while (entry!=0);      
+        Console.WriteLine($"The Sum is: {numbers.Sum()}");
+        Console.WriteLine($"The Average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
