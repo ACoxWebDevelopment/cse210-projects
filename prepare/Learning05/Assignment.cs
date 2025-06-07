c69d601e211781dc17e1fc40b869057bbf5971fa
Allan Cox <Allan.L.Coxrr@gmail.com>
2025-06-07 08:12:45 -0600

    inheritance practice

---
+++ prepare/Learning05/Assignment.cs
@@ -0,0 +1,18 @@
+class Assignment
+{
+    protected string _studentName;
+    protected string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+
+    }
+    public Assignment()
+    {}
+    public string GetSummary()
+    {
+        return ($"{_studentName} - {_topic}");
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        MathAssignment myMath = new MathAssignment("Herbert Hoover", "math", "fractions", "problems 1-25");
+        string mathHomework = myMath.GetHomeworkList();
+        Console.WriteLine(mathHomework);
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Maria Jones", "Writing", "Middle Earth by Maria Jones");
+        string writingHomework = myWritingAssignment.GetWritingInformation();
+        Console.WriteLine();
+        Console.WriteLine(writingHomework);
     }
-}
+
+} 
